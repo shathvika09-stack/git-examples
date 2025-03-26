3fc462a710f259b13d22b675efe113e1db5ccd53
poojitha <shathvika774@gmail.com>
2025-03-26 13:11:07 +0530

    committing changes to master branch

--- feature1.java
+++ feature1.java
@@ -2,4 +2,7 @@ public class feature1 {
     public static void main(String[] args) {
         System.out.println(x:"feature1");
     }
+    public void feature1() {
+        System.out.println("feature1");
+    }
 }
